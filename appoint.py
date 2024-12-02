edc23dedb9d55f589aa182af4f07375a06cc4f8b
santiago pabon <santiago.pabon72@gmail.com>
2024-12-01 19:57:35 -0700

    getter-method-created

--- appoint.py
+++ appoint.py
@@ -5,3 +5,20 @@ class Appointment:
         self.appt_type = appt_type
         self.day_of_week = day_of_week
         self.start_time_hour = start_time_hour
+
+
+    def client_name_getter(self):
+        return self.client_name
+    
+    def client_phone_getter(self):
+        return self.client_phone
+
+    def appt_type_getter(self):
+        return self.appt_type
+
+
+    def day_of_week_getter(self):
+        return self.day_of_week
+
+    def start_time_hour_getter(self):
+        return self.start_time_hour                
