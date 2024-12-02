df1bdc6936e7297d8f8145656867a103ac2faeb0
santiago pabon <santiago.pabon72@gmail.com>
2024-12-01 19:52:13 -0700

    constructor-made

---
+++ appoint.py
@@ -0,0 +1,7 @@
+class Appointment:
+    def __init__(self,client_name, client_phone, appt_type, day_of_week, start_time_hour):
+        self.client_name = client_name
+        self.client_phone = client_phone
+        self.appt_type = appt_type
+        self.day_of_week = day_of_week
+        self.start_time_hour = start_time_hour
