dc94e6c4bca0bc2e90a81c050b2149b9a238200e
santiago pabon <santiago.pabon72@gmail.com>
2024-12-01 20:04:08 -0700

    setter-methods-created

--- appoint.py
+++ appoint.py
@@ -21,4 +21,21 @@ class Appointment:
         return self.day_of_week
 
     def start_time_hour_getter(self):
-        return self.start_time_hour                
+        return self.start_time_hour
+    
+
+    def client_name_setter(self,client_name_value):
+        self.client_name = client_name_value
+        return self.client_name
+    
+    def client_phone_setter(self,client_phone_value):
+        self.client_phone = client_phone_value
+        return self.client_phone
+
+    def appt_type_setter(self,appt_type_value):
+        self.appt_type = appt_type_value
+        return self.appt_type
+    
+     
+
+                   
