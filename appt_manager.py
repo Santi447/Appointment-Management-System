34b25bd8f908ac59a007101003d9eaeb5886f60b
Gianna Natanauan <g.natanauan626@gmail.com>
2024-12-05 16:46:31 -0700

    Appointment Class - Gianna

--- appt_manager.py
+++ appt_manager.py
@@ -1,3 +1,22 @@
+from appoint import Appointment
+
+client_name = ""
+client_phone = ""
+appt_type = 0
+DAY_OF_WEEK = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+START_TIME_HOUR = [9, 10, 11, 12, 13, 14, 15, 16]
+appointments_list = []
+
+def create_weekly_calendar(appointment_list):
+    appointment_list.clear()
+    for day in DAY_OF_WEEK:
+        for hour in START_TIME_HOUR:
+            appt = Appointment(day,hour)
+            appointments_list.append(appt)
+
+def load_scheduled_appointments():
+    pass
+
 def print_menu():
     selection_invalid = True
     while(selection_invalid):
@@ -22,3 +41,85 @@ def print_menu():
         else:
             print("\nInvalid option\n")
     return selection
+
+def find_appointment_by_time():
+    pass
+
+def show_appointments_by_name():
+    pass
+
+def show_appointments_by_day():
+    pass
+
+def change_appointment_by_day_time():
+    pass
+
+def calculate_fees_per_day():
+    pass
+
+def calculate_weekly_fees():
+    pass
+
+def save_scheduled_appointments():
+    pass
+
+def schedule_appointment():
+    pass
+
+def main():
+    print("Starting the Appointment Manager System")
+    create_weekly_calendar(appointments_list)
+    prev_sched_appt = input("Would you like to load previously scheduled appointments from a file (Y/N)? ").strip().lower()
+    if prev_sched_appt == "y":
+        load_scheduled_appointments()
+        print("loaded")
+    
+    selection = 0
+    while selection != 9:
+        print()
+        selection = print_menu()
+        if selection == 1:
+            print("\n** Schedule an appointment **")
+            day = input("What day: ").strip().title()
+            start_hour = int(input("Enter start hour (24 hour clock): "))
+
+            if (day in DAY_OF_WEEK and start_hour in START_TIME_HOUR): 
+                i = 0
+                for appt in appointments_list:
+                    time = appt.get_start_time_hour()
+                    appt_day = appt.get_day_of_week()
+                    if time == start_hour and appt_day == day:
+                        type = appt.get_appointment_type()
+                        if type == 0:
+                            client_name = input("Client Name: ").strip()
+                            client_phone = input("Client Phone: ").strip()
+                            print("Appointment types")
+                            print("1: Mens Cut $40, 2: Ladies Cut $60, 3: Mens Colouring $40, 4: Ladies Colouring $80")
+                            appt_type = int(input("Type of Appointment: "))
+                            appointments_list[i] = Appointment(day, time, client_name, client_phone, appt_type)
+                            print(f"Ok, {client_name}'s appointment is scheduled!")
+                        else:
+                            print("Sorry that time slot is booked already!")
+                    i += 1
+
+            else:
+                print("Sorry that time slot is not in the weekkly calendar!")
+        elif selection == 2:
+            pass
+        elif selection == 3:
+            pass
+        elif selection == 4:
+            pass
+        elif selection == 5:
+            pass
+        elif selection == 6:
+            pass
+        elif selection == 7:
+            pass
+        elif selection == 9:
+            print("\n** Exit the system **")
+            save = input("Would you like to save all scheduled appointments to a file (Y/N)? ").strip().lower()
+            if save == "y":
+                save_scheduled_appointments()
+            print("Good Bye!")
+main()
