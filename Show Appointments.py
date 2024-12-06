9a004ca59f9ae83bac7f9ffb43495bfa42c1b5fb
Cauy Tuson <cauy.tuson@edu.sait.ca>
2024-12-05 16:58:12 -0800

    Show appointments by day v1

---
+++ Show Appointments.py
@@ -0,0 +1,18 @@
+
+
+
+def show_appointments_by_day():
+
+    print('** Print Calander for a specific day **')
+    
+    input_date = input('Enter day of week: ').capitalize()
+    print(f'Appointments for {input_date}')
+
+    
+    def __str__():
+
+        print('Client Name         Phone          Day       Start     End       Type')
+        print(f'-'*70)
+        return
+    
+
